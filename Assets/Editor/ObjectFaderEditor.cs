50ba3b83bbf673dd02a0b5dc4d5f8a41fd6905be
Stanton-TransfrVR <129227182+Stanton-TransfrVR@users.noreply.github.com>
2025-07-31 17:20:20 -0400

    Object fading
    A new ObjectFader script (and accompanying shader) allows for game objects to fade in/out as needed. An editor script was also created in order to test via Inspector buttons.

---
+++ Assets/Editor/ObjectFaderEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ObjectFader))]
+public class ObjectFaderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GUILayout.Space(10);
+        GUILayout.Label("Test Fade Controls", EditorStyles.boldLabel);
+
+        ObjectFader fader = (ObjectFader)target;
+
+        GUI.enabled = Application.isPlaying;
+
+        if (GUILayout.Button("⤴ Fade In"))
+        {
+            fader.FadeIn();
+        }
+
+        if (GUILayout.Button("⤵ Fade Out"))
+        {
+            fader.FadeOut();
+        }
+
+        GUI.enabled = true;
+    }
+}
